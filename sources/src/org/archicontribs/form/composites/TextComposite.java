7b15aa0b1fff806c2da58e1711d477a02f6fd11c
herve91 <herve.jouin@gmail.com>
2017-08-21 13:45:24 +0200

    added Excel fields

--- sources/src/org/archicontribs/form/composites/TextComposite.java
+++ sources/src/org/archicontribs/form/composites/TextComposite.java
@@ -2,6 +2,7 @@ package org.archicontribs.form.composites;
 
 import org.archicontribs.form.editors.AlignmentEditor;
 import org.archicontribs.form.editors.ColorEditor;
+import org.archicontribs.form.editors.ComboEditor;
 import org.archicontribs.form.editors.FontEditor;
 import org.archicontribs.form.editors.SizeEditor;
 import org.archicontribs.form.editors.StringEditor;
@@ -11,18 +12,19 @@ import org.eclipse.swt.layout.FormLayout;
 import org.eclipse.swt.widgets.Composite;
 
 public class TextComposite extends Composite {
-	private StringEditor            nameEditor;         // name
-    private StringEditor            textEditor;			// text
-	private SizeEditor              sizeEditor;         // x, y, width, height
-	private ColorEditor             colorEditor;        // foreground, background
-	private FontEditor				fontEditor;			// font, fontBold, fontItalic
-	private StringEditor		    tooltipEditor;      // tooltip
-	private AlignmentEditor         alignmentEditor;    // alignment
+	private StringEditor            nameEditor;          // name
+    private StringEditor            textEditor;			 // text
+	private SizeEditor              sizeEditor;          // x, y, width, height
+	private ColorEditor             colorEditor;         // foreground, background
+	private FontEditor				fontEditor;			 // font, fontBold, fontItalic
+	private StringEditor		    tooltipEditor;       // tooltip
+	private AlignmentEditor         alignmentEditor;     // alignment
 	
-    private StyledText txtExcelSheet        = null;         // excelSheet
-    private StyledText txtExcelCell         = null;         // excelCell
-    private CCombo     txtExcelCellType     = null;         // excelCellType
-    private CCombo     txtExcelDefault      = null;         // excelDefault
+	private StringEditor            excelSheetEditor;    // excelSheet
+	private StringEditor            excelCellEditor;     // excelCell
+	private ComboEditor             excelCellTypeEditor; // excelCellType
+	private ComboEditor             excelDefaultEditor;  //excelDefault
+
 
 	public TextComposite(Composite parent, int style) {
 		super(parent, style);
@@ -63,12 +65,50 @@ public class TextComposite extends Composite {
 		tooltipEditor.setProperty("tooltip");
 		tooltipEditor.mustSetControlTolltip(true);
 		
-        // tooltip
+        // alignment
         alignmentEditor = new AlignmentEditor(this);
         alignmentEditor.setPosition(tooltipEditor.getControl());
         alignmentEditor.setItems(new String[] {"", "left", "center", "right"});
         alignmentEditor.setTooltipText("Choose the alignment.\n\nDefault: left.");
         alignmentEditor.setProperty("alignment");
+        
+        // excelSheet
+        excelSheetEditor = new StringEditor(this);
+        excelSheetEditor.setPosition(alignmentEditor.getControl());
+        excelSheetEditor.setLabel("Excel sheet:");
+        excelSheetEditor.setProperty("excelsheet");
+        excelSheetEditor.setTooltipText("Name of the Excel sheet where the variable should be exported to.\n\nIf this field is left blank, then the variable will not be exported to Excel, even if the others Excel related field are set.");
+        
+        // excelCell
+        excelCellEditor = new StringEditor(this);
+        excelCellEditor.setPosition(excelSheetEditor.getControl());
+        excelCellEditor.setLabel("Excel cell:");
+        excelCellEditor.setProperty("excelcell");
+        excelCellEditor.setTooltipText("Adress of the Excel cell where the variable should be exported to (like A3 or D14).\n\nIf the \"Excel sheet\" field is not set, then the variable will not be exported to Excel even if this field is set.");
+        
+        // excelCellType
+        excelCellTypeEditor = new ComboEditor(this);
+        excelCellTypeEditor.setPosition(excelCellEditor.getControl());
+        excelCellTypeEditor.setLabel("Excel type:");
+        excelCellTypeEditor.setProperty("exceltype");
+        excelCellTypeEditor.setItems(new String[] {"", "string", "boolean", "numeric", "formula"});
+        excelCellTypeEditor.setTooltipText("Type of the Excel cell.\n\nDefault: string");
+        
+        // excelDefault
+        excelDefaultEditor = new ComboEditor(this);
+        excelDefaultEditor.setPosition(excelCellTypeEditor.getControl());
+        excelDefaultEditor.setLabel("Excel default:");
+        excelDefaultEditor.setProperty("exceldefault");
+        excelDefaultEditor.setItems(new String[] {"", "blank", "zero", "delete"});
+        excelDefaultEditor.setTooltipText("Behaviour of the plugin when exporting an empty value:\n"+
+                "   - blank : a blank cell will be created (ie a cell with no content)\n"+
+                "   - zero : a cell with a zero value in it:\n"+
+                "                - 0 for numeric cells\n"+
+                "                - empty string for string and formula cells\n"+
+                "                - false for boolean cells\n"+
+                "   - delete : the cell will be deleted.\n"+
+                "\n"+
+                "Default: blank");
 	}
 	
     public void set(String key, int value) throws RuntimeException {
