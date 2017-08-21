bef6d0e160b8fdbf4545318fe26e1ba4ac105ba8
herve91 <herve.jouin@gmail.com>
2017-08-21 13:55:28 +0200

    add labelText parameter

--- sources/src/org/archicontribs/form/composites/TextComposite.java
+++ sources/src/org/archicontribs/form/composites/TextComposite.java
@@ -6,8 +6,6 @@ import org.archicontribs.form.editors.ComboEditor;
 import org.archicontribs.form.editors.FontEditor;
 import org.archicontribs.form.editors.SizeEditor;
 import org.archicontribs.form.editors.StringEditor;
-import org.eclipse.swt.custom.CCombo;
-import org.eclipse.swt.custom.StyledText;
 import org.eclipse.swt.layout.FormLayout;
 import org.eclipse.swt.widgets.Composite;
 
@@ -34,14 +32,14 @@ public class TextComposite extends Composite {
 	
 	private void createContent() {
 		// name
-		nameEditor = new StringEditor(this);
+		nameEditor = new StringEditor(this, "Name:");
 		nameEditor.setPosition(0);
 		nameEditor.setProperty("name");
 		nameEditor.mustSetTreeItemText(true);
 		nameEditor.treeItemTextPrefix("Name: ");
 		
 		// text
-		textEditor = new StringEditor(this);
+		textEditor = new StringEditor(this, "Text:");
 		textEditor.setPosition(nameEditor.getControl());
 		textEditor.setProperty("text");
 		textEditor.mustSetTreeItemText(true);
@@ -52,52 +50,48 @@ public class TextComposite extends Composite {
 		sizeEditor.setPosition(textEditor.getControl());
 		        
 		// Background
-		colorEditor = new ColorEditor(this);
+		colorEditor = new ColorEditor(this, "Color:");
 		colorEditor.setPosition(sizeEditor.getControl());
 		
 		// font, fontBold, fontItalic
-		fontEditor = new FontEditor(this);
+		fontEditor = new FontEditor(this, "Font");
 		fontEditor.setPosition(colorEditor.getControl());
 		
 		// tooltip
-		tooltipEditor = new StringEditor(this, 5);
+		tooltipEditor = new StringEditor(this, "Tooltip:", 5);
 		tooltipEditor.setPosition(fontEditor.getControl());
 		tooltipEditor.setProperty("tooltip");
 		tooltipEditor.mustSetControlTolltip(true);
 		
         // alignment
-        alignmentEditor = new AlignmentEditor(this);
+        alignmentEditor = new AlignmentEditor(this, "Alignment:");
         alignmentEditor.setPosition(tooltipEditor.getControl());
         alignmentEditor.setItems(new String[] {"", "left", "center", "right"});
         alignmentEditor.setTooltipText("Choose the alignment.\n\nDefault: left.");
         alignmentEditor.setProperty("alignment");
         
         // excelSheet
-        excelSheetEditor = new StringEditor(this);
+        excelSheetEditor = new StringEditor(this, "Excel sheet:");
         excelSheetEditor.setPosition(alignmentEditor.getControl());
-        excelSheetEditor.setLabel("Excel sheet:");
         excelSheetEditor.setProperty("excelsheet");
         excelSheetEditor.setTooltipText("Name of the Excel sheet where the variable should be exported to.\n\nIf this field is left blank, then the variable will not be exported to Excel, even if the others Excel related field are set.");
         
         // excelCell
-        excelCellEditor = new StringEditor(this);
+        excelCellEditor = new StringEditor(this, "Excel cell:");
         excelCellEditor.setPosition(excelSheetEditor.getControl());
-        excelCellEditor.setLabel("Excel cell:");
         excelCellEditor.setProperty("excelcell");
         excelCellEditor.setTooltipText("Adress of the Excel cell where the variable should be exported to (like A3 or D14).\n\nIf the \"Excel sheet\" field is not set, then the variable will not be exported to Excel even if this field is set.");
         
         // excelCellType
-        excelCellTypeEditor = new ComboEditor(this);
+        excelCellTypeEditor = new ComboEditor(this, "Excel type:");
         excelCellTypeEditor.setPosition(excelCellEditor.getControl());
-        excelCellTypeEditor.setLabel("Excel type:");
         excelCellTypeEditor.setProperty("exceltype");
         excelCellTypeEditor.setItems(new String[] {"", "string", "boolean", "numeric", "formula"});
         excelCellTypeEditor.setTooltipText("Type of the Excel cell.\n\nDefault: string");
         
         // excelDefault
-        excelDefaultEditor = new ComboEditor(this);
+        excelDefaultEditor = new ComboEditor(this, "Excel default:");
         excelDefaultEditor.setPosition(excelCellTypeEditor.getControl());
-        excelDefaultEditor.setLabel("Excel default:");
         excelDefaultEditor.setProperty("exceldefault");
         excelDefaultEditor.setItems(new String[] {"", "blank", "zero", "delete"});
         excelDefaultEditor.setTooltipText("Behaviour of the plugin when exporting an empty value:\n"+
