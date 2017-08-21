96038c0f2e3d0c5676b8aee076ad6028a0118612
Hervé <herve.jouin@gmail.com>
2017-08-21 21:45:34 +0200

    new

---
+++ sources/src/org/archicontribs/form/composites/CompositeInterface.java
@@ -0,0 +1,7 @@
+package org.archicontribs.form.composites;
+
+public interface CompositeInterface {
+	public void setVisible(boolean mustBeVisible);
+	public void setData(String key, Object value);
+	public void set(String key, Object value);
+}
