b241b26b43b6a6d952dba8a6440a6822baa43d41
Hervé <herve.jouin@gmail.com>
2017-09-28 21:59:21 +0200

    add wait cursor

--- sources/src/org/archicontribs/form/FormDialog.java
+++ sources/src/org/archicontribs/form/FormDialog.java
@@ -135,6 +135,9 @@ public class FormDialog extends Dialog {
     public final static Color    blackColor       	      = new Color(display, 0, 0, 0);
     public final static Color    whiteColor       	      = new Color(display, 255, 255, 255);
     
+	public static final Cursor   CURSOR_WAIT              = display.getSystemCursor(SWT.CURSOR_WAIT);
+	public static final Cursor   CURSOR_ARROW             = display.getSystemCursor(SWT.CURSOR_ARROW);
+    
     public final static int      editorLeftposition       = 110;
     public final static int      editorBorderMargin       = 10;
     public final static int      editorVerticalMargin     = 10;
@@ -207,7 +210,10 @@ public class FormDialog extends Dialog {
         formVarList.reset();
 
         try {
+        	display.getActiveShell().setCursor(CURSOR_WAIT);
+        	
         	formDialog = new Shell(getParent(), SWT.DIALOG_TRIM | SWT.APPLICATION_MODAL);
+        	formDialog.setCursor(CURSOR_WAIT);
         	
             TreeItem formTreeItem = null;
             // If the dialog is ran against no EObject, then we switch to edit mode 
@@ -216,9 +222,15 @@ public class FormDialog extends Dialog {
             if ( editMode ) {
             	// in edit mode, we show up the graphical editor
             	formTreeItem = createPropertiesDialog(jsonForm);
+            	propertiesDialog.setCursor(CURSOR_WAIT);
             }
             
             jsonParser.createForm(jsonForm, formDialog, formTreeItem);
+            
+            // if we are in form mode, then we expand the name of the form
+            if ( !editMode ) {
+            	formDialog.setText(FormVariable.expand(formDialog.getText(), selectedObject));
+            }
 
             // we create one CTabItem per tab array item
             CTabFolder tabFolder = (CTabFolder)formDialog.getData("tab folder");
@@ -234,6 +246,11 @@ public class FormDialog extends Dialog {
                     	tabTreeItem = new TreeItem(formTreeItem, SWT.NONE);
                     
                     CTabItem tabItem = jsonParser.createTab(jsonTab, tabFolder, tabTreeItem);
+                    
+                    // if we are in form mode, then we expand the tab name in case it contains variables
+                    if ( !editMode ) {
+                    	tabItem.setText(FormVariable.expand(tabItem.getText(), selectedObject));
+                    }
                     
                     JSONArray controls = jsonParser.getJSONArray(jsonTab, "controls");
                     if ( controls != null ) {
@@ -286,11 +303,13 @@ public class FormDialog extends Dialog {
             FormDialog.popup(Level.ERROR, "Wrong key type in the configuration file \"" + configFilename + "\"", e);
             if (formDialog != null)
             	formDialog.dispose();
+        	display.getActiveShell().setCursor(CURSOR_ARROW);
             return;
         } catch (RuntimeException e) {
             FormDialog.popup(Level.ERROR, "Please check your configuration file \"" + configFilename + "\"", e);
             if (formDialog != null)
             	formDialog.dispose();
+            display.getActiveShell().setCursor(CURSOR_ARROW);
             return;
         }
 
@@ -300,7 +319,11 @@ public class FormDialog extends Dialog {
         if ( selectedObject == null ) {
         	propertiesDialog.open();
             propertiesDialog.layout();
+            propertiesDialog.setCursor(CURSOR_ARROW);
         }
+        
+        display.getActiveShell().setCursor(CURSOR_ARROW);
+        formDialog.setCursor(CURSOR_ARROW);
     }
 
     /**
@@ -925,6 +948,7 @@ public class FormDialog extends Dialog {
                 	            }
                             }
                         }
+                        table.layout();
                         
                         control = (Control)table;
 	                	break;
